57271c0fc8a3b8019870b4c9165907ddaebdfaaf
moodgoza <moodghoz@gmail.com>
2023-08-17 16:34:11 +0300

    add search && inline-edit

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const tableBody = document.getElementsByTagName("TBODY")[0];
 const todoDiscription = document.getElementById("new-task");
 const addButton = document.getElementById("add");
+const searchButton = document.getElementById("search");
 const container = document.getElementById("container");
 const BASE_URL = "https://dummyjson.com/todos";
 const cont = document.getElementById("cont");
+const searchInput = document.getElementById("search-input");
+const numOfTodo = document.getElementById("numOfTodo");
 let deletedId = false;
 let page = 1;
 let todos = [];
 let myToken = "";
 container.hidden = true;
 
-// log in
+// log in api
 const login = async () => {
   let res = await fetch("https://dummyjson.com/auth/login", {
     method: "POST",
@@ -26,7 +29,7 @@ const login = async () => {
   return res;
 };
 
-// get all todos
+// get all todos api
 const getTodos = async (myToken) => {
   let res = await fetch(BASE_URL, {
     method: "GET",
@@ -35,11 +38,12 @@ const getTodos = async (myToken) => {
       "Content-type": "application/json",
     },
   });
+
   res = res.json();
   return res;
 };
 
-// add todo
+// add todo api
 const addTodo = async (todo) => {
   let res = await fetch(`${BASE_URL}/add`, {
     method: "POST",
@@ -54,9 +58,9 @@ const addTodo = async (todo) => {
   return res;
 };
 
-//change status
+//change status api
 const changeStatus = (todo) => {
-  console.log(todo);
+
   let res = fetch(`${BASE_URL}/${todo.id}`, {
     headers: {
       Authorization: `Bearer ${myToken}`,
@@ -65,12 +69,13 @@ const changeStatus = (todo) => {
     method: "PUT",
     body: JSON.stringify({ completed: !todo.completed }),
   });
+
   return res;
 };
 
-//delete Todo
+//delete Todo api
 const deleteTodo = (id) => {
-  console.log(id);
+
   let res = fetch(`${BASE_URL}/${id}`, {
     headers: {
       Authorization: `Bearer ${myToken}`,
@@ -78,134 +83,223 @@ const deleteTodo = (id) => {
     },
     method: "Delete",
   });
+
   return res;
 };
 
-const renderPage = () => {
+//update content api
+const updateContent = async(id, newContent) => {
+  let res = fetch(`${BASE_URL}/${id}`, {
+    headers: {
+      Authorization: `Bearer ${myToken}`,
+      "Content-Type": "application/json",
+    },
+    method: "PUT",
+    body: JSON.stringify({ todo: newContent }),
+  });
 
- 
-  if(page < 1 || (page - 1) * 7 >= todos.length)
-  {
+  return res;
+}
+//render todos in the container
+const renderPage = (todos, fromSerach = false) => {
+
+  if ((page < 1 || (page - 1) * 7 >= todos.length) && !fromSerach) {
     page = Math.max(1, page - 1);
     return;
   }
-  tableBody.innerHTML = ` <tr>
-  <td colspan="5"><hr /></td>
-</tr>`
-  for(let i = (page - 1) * 7; i < Math.min((page) * 7, todos.length); i++)
-  {
+
+  tableBody.innerHTML =
+  ` <tr>
+      <td colspan="5">
+        <hr />
+      </td>
+    </tr>`;
+
+  for (let i = (page - 1) * 7; i < Math.min(page * 7, todos.length); i++) {
     tableBody.innerHTML += genTodo(todos[i]);
   }
 
-  const pageNumber = document.querySelector('footer > div > span');
+  const pageNumber = document.querySelector("footer > div > span");
   pageNumber.innerHTML = `Page ${page}`;
-  const numOfTodo = document.getElementById("numOfTodo");
+
+ 
   numOfTodo.innerHTML = `${todos.length} Task`;
+
 };
 
+//move to the next page
 const rightArrow = () => {
   page++;
-  renderPage();
-}
+  renderPage(todos);
+};
 
+//move to the previous page
 const leftArrow = () => {
   page--;
-  renderPage();
-}
+  renderPage(todos);
+};
 
+//logging
 login().then(({ token }) => {
   myToken = token;
+
   container.hidden = false;
-  if(localStorage.getItem("todos") === null)
-  {
+
+  if (localStorage.getItem("todos") === null) {
     getTodos(token).then((data) => {
       todos = data.todos;
-      renderPage();
-      localStorage.setItem("todos", JSON.stringify(todos))
-      console.log(JSON.parse(localStorage.getItem("todos")))
-      
+
+      renderPage(todos);
+
+      localStorage.setItem("todos", JSON.stringify(todos));
     });
-  }
-  else 
-  {
+  } else {
     todos = JSON.parse(localStorage.getItem("todos"));
 
-    renderPage();
+    renderPage(todos);
   }
-  
 });
 
+// create new todo
 addButton.addEventListener("click", (event) => {
   addTodo({ todo: todoDiscription.value, completed: false, userId: 2 }).then(
     (todo) => {
-      if(page * 7 > todos.length)
-      tableBody.innerHTML += genTodo(todo);
+      if (page * 7 > todos.length) {
+        tableBody.innerHTML += genTodo(todo);
+      }
+
       todos.push(todo);
+
       localStorage.setItem("todos", JSON.stringify(todos));
+
       const numOfTodo = document.getElementById("numOfTodo");
+
       numOfTodo.innerHTML = `${todos.length} Task`;
     }
   );
 });
 
+// mark todo as done or unde done
 const changeTodoStatus = (id) => {
   const todo = todos.filter((t) => t.id === id)[0];
 
   changeStatus(todo).then((data) => {
-    console.log("status changed successfully", data.ok);
     todo.completed = !todo.completed;
+
     localStorage.setItem("todos", JSON.stringify(todos));
+
     const status = document.querySelector(`#todo${todo.id}  #status`);
+
     console.log(status);
+
     status.innerHTML = todo.completed ? "Completed" : "In progress";
 
     const changeButton = document.querySelector(
       `#todo${todo.id} #${!todo.completed ? "undo" : "done"}`
     );
-    console.log(todo.completed, changeButton);
+
     changeButton.innerHTML = !todo.completed ? "Done" : "Undo";
     changeButton.setAttribute("id", !todo.completed ? "done" : "undo");
-    console.log(todos)
   });
 };
 
+//confirm todo deletion by "YES"
 const confirmationYes = async () => {
   await deleteTodo(deletedId).then((data) => {
     console.log("todo deleted successfully", data.ok);
+
     const todo = document.getElementById(`todo${deletedId}`);
     const trHr = document.querySelector(`#todo${deletedId} + tr`);
+
     console.log(trHr);
+
     todo.remove();
     trHr.remove();
+
     todos = todos.filter((t) => t.id != deletedId);
+
     localStorage.setItem("todos", JSON.stringify(todos));
+
     const numOfTodo = document.getElementById("numOfTodo");
+
+    renderPage(todos);
+
     numOfTodo.innerHTML = `${todos.length} Task`;
   });
   document.getElementById("cont").style.display = "none";
 };
 
+//confirm todo deletion by "NO"
 const confirmationNo = () => {
   document.getElementById("cont").style.display = "none";
 };
 
+//show deletion confirmation
 const deleteTodoById = async (id) => {
   deletedId = id;
   document.getElementById("cont").style.display = "flex";
 };
 
 
+searchButton.addEventListener('click', (event) => {
+  const searchResult = todos.filter(t => String(t.todo).includes(String(searchInput.value)));
+  console.log(searchResult);
+  page = 1;
+  renderPage(searchResult, true);
+});
+
+numOfTodo.addEventListener('click', (event) => {
+  page = 1;
+  renderPage(todos);
+})
+const replaceToInput  = (id) => {
+  const todoInput = document.querySelector(`#todo${id} #desc #todo-input`);
+  const todoP = document.querySelector(`#todo${id} #desc #todo-p`);
+  todoInput.style.display = "block"
+  todoP.style.display = "none"
+  document.getElementById("edit").style.display = 'inline';
+}
+
+const replaceToP = (id) => {
+  const todoInput = document.querySelector(`#todo${id} #desc #todo-input`);
+  const todoP = document.querySelector(`#todo${id} #desc #todo-p`);
+  console.log(String(todoInput.value), String(todoP.innerHTML))
+  if(String(todoInput.value) !== String(todoP.innerHTML))
+  {
+    return false;
+  }
+  todoInput.style.display = "none"
+  todoP.style.display = "block"
+  document.getElementById("edit").style.display = 'none';
+}
+
+const editContent = (id) => {
+  console.log(id)
+  const todoInput = document.querySelector(`#todo${id} #desc #todo-input`);
+  const todoP = document.querySelector(`#todo${id} #desc #todo-p`);
+  updateContent(id, todoInput.value)
+  .then(() => {
+    let todo = todos.filter(t => t.id === id)[0];
+    todo.todo = todoInput.value
+    todoP.innerHTML = todo.todo;
+    todoInput.style.display = "none"
+    todoP.style.display = "block"
+    localStorage.setItem("todos", JSON.stringify(todos))
+    document.getElementById("edit").style.display = 'none';
+  })
+}
+
 //generate todo
 const genTodo = (todo) => {
   return `
       <tr id=todo${todo.id}>
       <td><span id="todoId">${todo.id}</span></td>
-      <td><span id="desc">${todo.todo}</span></td>
+      <td><span id="desc"><input onblur="replaceToP(${todo.id})" id='todo-input' value="${todo.todo}"> <p onclick="replaceToInput(${todo.id})" id="todo-p">${todo.todo}</p> <span id="edit"><i onclick="editContent(${todo.id})" class="fa-solid fa-pen-to-square"></i></span></span></td>
       <td><span id=status>${
         todo.completed ? "complete" : "In progress"
       }</span></td>
       <td><span id=userId>${todo.userId}</span></td>
-      <td><span><button id=${
+      <td><span id="todo_action"><button id=${
         !todo.completed ? "done" : "undo"
       } onclick="changeTodoStatus(${todo.id})">${
     !todo.completed ? "Done" : "Undo"
